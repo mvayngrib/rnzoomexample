d37a50139a4458aa2f108cbb789f54803d8d9744
Mark Vayngrib <mark@tradle.io>
2020-10-28 19:38:51 -0400

    feat: add enroll/authenticate example

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Platform,
   TouchableHighlight,
@@ -6,132 +6,125 @@ import {
   ScrollView,
   View,
   Text,
-  Image
-} from "react-native";
+  Image,
+} from 'react-native'
 
-import Zoom from "react-native-facetec-zoom";
-import ZOOM_APP_TOKEN from "./license-key";
+import Zoom from 'react-native-facetec-zoom'
+import deviceKeyIdentifier from './device-key-identifier'
 
-Zoom.preload();
+const BigText = ({ children, style }) => <Text style={[styles.bigText, style]}>{children}</Text>
 
-const bigText = (text, style) => (
-  <Text style={[styles.bigText, style]}>{text}</Text>
-);
-const hugeText = (text, style) => (
-  <Text style={[styles.hugeText, style]}>{text}</Text>
-);
-const createButton = ({ text, ...props }) => (
+const Button = ({ children, ...props }) => (
   <TouchableHighlight {...props} style={styles.button}>
-    {hugeText(text, styles.buttonText)}
+    <Text style={[styles.hugeText, styles.buttonText]}>{children}</Text>
   </TouchableHighlight>
-);
+)
 
-const prettify = obj => (obj ? JSON.stringify(obj, null, 2) : "");
+const prettify = (obj) => (obj ? JSON.stringify(obj, null, 2) : '')
+// don't actually do this
+const getFaceID = () => String(Math.random()).split('.')[1]
 
 export default class App extends React.Component {
-  state = {};
-  verifyLiveness = async () => {
+  state = {}
+  init = async () => {
     const { success, status } = await Zoom.initialize({
-      appToken: ZOOM_APP_TOKEN,
+      deviceKeyIdentifier,
       showPreEnrollmentScreen: false,
       showUserLockedScreen: false,
       showRetryScreen: false,
       topMargin: 200,
       // sizeRatio: 0.7,
-      centerFrame: false
-    });
+      centerFrame: false,
+      returnBase64: false,
+    })
 
     if (!success) {
       // see constants.js SDKStatus for explanations of various
       // reasons why initialize might not have gone through
-      throw new Error(`failed to init. SDK status: ${status}`);
+      throw new Error(`failed to init. SDK status: ${status}`)
     }
 
-    // launch Zoom's verification process
-    return await Zoom.verify({
-      // no options at this point
-    });
-  };
-  render = () => {
-    const button = createButton({
-      key: "button",
-      text: "Verify Liveness",
-      onPress: async () => {
-        try {
-          this.setState({
-            result: await this.verifyLiveness()
-          });
-        } catch (error) {
-          this.setState({ error });
-        }
-      }
-    });
-    let result = this.renderResult();
-    let error = this.state.error && (
-      <View>
-        {bigText("Error:")}
-        {this.renderError()}
-      </View>
-    );
+    this.initialized = true
+  }
 
+  enroll = async () => {
+    if (!this.initialized) await this.init()
+
+    const faceID = getFaceID()
+    this.setState({ faceID })
+
+    // launch Zoom's enrollment process
+    return Zoom.enroll({
+      id: faceID,
+    }).then(this.setResult, this.setError)
+  }
+
+  authenticate = async () => {
+    if (!this.initialized) await this.init()
+
+    // launch Zoom's enrollment process
+    return Zoom.authenticate({
+      id: this.state.faceID,
+    }).then(this.setResult, this.setError)
+  }
+
+  verifyLiveness = async () => {
+    if (!this.initialized) await this.init()
+
+    // launch Zoom's verification process
+    return Zoom.verifyLiveness().then(this.setResult, this.setError)
+  }
+
+  setResult = (result) => this.setState({ result })
+  setError = (error) => this.setState({ error })
+
+  render = () => {
     return (
       <View style={styles.container}>
-        {bigText("Variants:")}
-        {button}
-        {bigText("Result:")}
-        {result}
-        {error}
+        <BigText>Operations:</BigText>
+        <Button onPress={this.verifyLiveness}>Verify Liveness</Button>
+        <Button onPress={this.enroll}>Enroll</Button>
+        {this.state.faceID && <Button onPress={this.authenticate}>Authenticate</Button>}
+        {this.state.result && (
+          <>
+            <BigText>Result:</BigText>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+              <BigText>{prettify(this.state.result)}</BigText>
+            </ScrollView>
+          </>
+        )}
+        {this.state.error && (
+          <>
+            <BigText>Error:</BigText>
+            <BigText>{prettify(this.state.error)}</BigText>
+          </>
+        )}
       </View>
-    );
-  };
-  renderResult = () => {
-    const { result } = this.state;
-    if (!result) return;
-
-    const { faceMetrics = {} } = result;
-    const { facemap, auditTrail = [] } = faceMetrics;
-    const images = auditTrail.map((uri, i) => (
-      <Image key={`image${i}`} style={styles.image} source={{ uri }} />
-    ));
-    return (
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {bigText(prettify(result))}
-        {images}
-      </ScrollView>
-    );
-  };
-  renderError = () => {
-    return (
-      this.state.error && (
-        <View key="error">{bigText(prettify(this.state.error))}</View>
-      )
-    );
-  };
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   scrollContainer: {},
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
-    padding: 10
+    padding: 10,
   },
   buttonText: {
-    color: "blue"
+    color: 'blue',
+    fontSize: 30,
   },
   bigText: {
-    fontSize: 20
-  },
-  hugeText: {
-    fontSize: 30
+    fontSize: 20,
   },
   image: {
     flex: 1,
-    resizeMode: "contain",
+    resizeMode: 'contain',
     minWidth: 300,
-    minHeight: 300
-  }
-});
+    minHeight: 300,
+  },
+})
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 See example code in `src/App.js`
 
 1. run `yarn` or `npm install` to install dependencies
-2. download and copy `ZoomAuthenticationHybrid.framework` to `ios/`
-3. copy your token to `license-key.json`, e.g.:
+2. [download](https://dev.facetec.com/downloads) the sdk and copy `FaceTecSDK.framework` to `ios/`
+3. copy your deviceKeyIdentifier to `device-key-identifier.json`, e.g.:
 
 ```json
 "abcdefg"
